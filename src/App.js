093d8acebfad37bc586801811468f021df634841
houssem-mbarki <hassou23955@gmail.com>
2021-10-20 09:50:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "houssem mbarki",
+      bio: "studying in go my code",
+      profession: "Full stack Web developper",
+
+      show: true,
+      count: 0,
+    };
+  }
+  handleShowPerson = () => this.setState({ show: !this.state.show, count: 0 });
+  counteur = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  componentDidMount() {
+    this.CD = setInterval(() => this.counteur(), 1000);
+  }
+  render() {
+    if (this.state.show)
+      return (
+        <div className="App">
+          <p>
+            Hello my name is {this.state.fullName} I'm in high spirits
+            {this.state.bio} I'm a {this.state.profession} .
+          </p>
+          <img src="/derulo.jpg" alt="it me " /> <br />
+          <button onClick={this.handleShowPerson}>click me</button>
+          <p>Timer : {this.state.count} second(s)</p>
+        </div>
+      );
+    else
+      return (
+        <div className="App">
+          <h1>Hello world</h1>
+          <button onClick={this.handleShowPerson}>Hello</button>
+        </div>
+      );
+  }
 }
 
 export default App;
